2b981d69d86765ca575bf44906ccd320b9135a7f
jatin-excellence <jatin_mehandiratta@excellencetechnologies.in>
2021-02-25 10:44:27 +0530

    jqueryPlugins edited

--- jqueryPlugins1.php
+++ jqueryPlugins1.php
@@ -6,6 +6,7 @@
     <script src="js/jquery-1.6.js" type="text/javascript"></script>
     <script type="text/javascript" src="js/jquery.jqzoom-core.js"></script>
     <link rel="stylesheet" type="text/css" href="css/jquery.jqzoom.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script type="text/javascript">
         $(document).ready(function() {
             $('.MYCLASS').jqzoom();
@@ -43,21 +44,21 @@
 
 <body>
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 m-4">
+        <div class="row mt-2 mb-3">
+            <div class="col-md-12 mr-4">
                 <a href="images/bigImage.jpg" class="MYCLASS" title="MYTITLE">
                     <img src="images/download.jpeg" title="IMAGE TITLE">
                 </a>
             </div>
         </div>
-        <div class="row">
+        <div class="row mt-3 mb-3">
             <div class="col-md-12">
                 <a href="images/bigImage.jpg" class="jqzoom2" title="JQZoom">
                     <img src="images/download.jpeg" title="JQZoom" alt="JQZoom" />
                 </a>
             </div>
         </div>
-        </div>
+    </div>
 </body>
 
 </html>
--- jqueryPlugins3.php
+++ jqueryPlugins3.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Sign Up Form</title>
+    <title>Fancy Box</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
